310a7adb77eba65510389f6a113da61efb08b727
Jeon Seung Kyu <jsgcys0815@gmail.com>
2024-07-04 23:27:17 +0900

    recipe_ingredient, fridge, ingredients DB 생성, "전체 재료" 초기 데이터 입력 endpoint 구현

--- src/models/ingredient.js
+++ src/models/ingredient.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
 const IngredientSchema = new mongoose.Schema({
-  recipe : { type: String, required: true},
-  name : { type: String, required: true},
-  amount : { type: String, required: true},
-  recipe_id : { type: mongoose.Schema.Types.ObjectId, ref: "recipe"}
+  name : { type: String, required: true}
 });
 
 const Ingredient = mongoose.model("Ingredient", IngredientSchema);
--- src/routes/initRouter.js
+++ src/routes/initRouter.js
@@ -5,46 +5,57 @@ import recipe_ingredients from '../js/recipe_ingredients.js';
 import recipe_process from '../js/recipe_process.js';
 import recipes from '../js/recipes.js';
 
-import Ingredient from "../models/Ingredient.js";
+import RecipeIngredient from "../models/recipe_ingredient.js";
 import Process from '../models/Process.js';
 import Recipe from '../models/Recipe.js';
+import Ingredient from '../models/ingredient.js';
 
 router.get('/', (req, res) => {
   res.send("GET /init TEST");
 });
 
-router.get('/ingredient', async (req, res) => {
+router.get('/recipe-ingredient', async (req, res) => {
   try {
-    await Ingredient.insertMany(recipe_ingredients);
+    await RecipeIngredient.insertMany(recipe_ingredients);
+    res.send("초기 데이터 입력 완료");
   } catch (error) {
     console.error(error);
     res.status(500).send("데이터 입력 중 오류 발생");
   }
-
-  res.send("초기 데이터 입력 완료");
 });
 
 router.get('/process', async (req, res) => {
   try {
 		await Process.insertMany(recipe_process);
+    res.send("초기 데이터 입력 완료");
   } catch (error) {
     console.error(error);
     res.status(500).send("데이터 입력 중 오류 발생");
   }
-
-  res.send("초기 데이터 입력 완료");
 });
 
 router.get('/recipe', async (req, res) => {
   try {
 		await Recipe.insertMany(recipes);
+    res.send("초기 데이터 입력 완료");
   } catch (error) {
     console.error(error);
     res.status(500).send("데이터 입력 중 오류 발생");
   }
-
-  res.send("초기 데이터 입력 완료");
 });
+
+router.get('/ingredients', async (req, res) => {
+  try {
+    const ingredients = await RecipeIngredient.distinct('name');
+    const objectsArray = ingredients.map(item => ({ name: item }));
+    await Ingredient.insertMany(objectsArray);
+
+    res.send("초기 데이터 입력 완료");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("데이터 입력 중 오류 발생");
+  }
+})
 
 
 export default router;
